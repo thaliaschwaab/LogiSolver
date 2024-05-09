25998d6bd313797784a5a546575498b5c3913c8b
Thalia Schwaab <thalia.danieli@gmail.com>
2024-05-09 09:32:11 -0300

    learn base

---
+++ controllers/pages/TruthTable.js
@@ -0,0 +1,11 @@
+function checkAnswer(answer, resultId) {
+  if (answer === 'Verdadeiro' || answer === 'F' || answer === 'True') {
+    var color = '#89CF7D';
+    document.getElementById(resultId).innerHTML = 'A resposta está <b>correta</b>.';
+    document.getElementById(resultId).style.color = color;
+  } else {
+    var color = '#E35D79';
+    document.getElementById(resultId).innerHTML = 'A resposta está <b>incorreta</b>.';
+    document.getElementById(resultId).style.color = color;
+  }
+}
