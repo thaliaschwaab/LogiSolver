f0d9982f5903cc4de92945153dd82c05815b1348
Thalia Schwaab <thalia.danieli@gmail.com>
2024-06-17 00:41:50 -0300

    classification

--- controllers/pages/Classification.js
+++ controllers/pages/Classification.js
@@ -1,5 +1,5 @@
 function checkAnswer(answer, resultId) {
-  if (answer === 'Verdadeiro' || answer === 'V' || answer === 'False') {
+  if (answer === 'Falso' || answer === 'V' || answer === 'False') {
     var color = '#89CF7D';
     document.getElementById(resultId).innerHTML = 'A resposta está <b>correta</b>.';
     document.getElementById(resultId).style.color = color;
